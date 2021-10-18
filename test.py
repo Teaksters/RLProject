2c5896794939c709864cf5eab5cc5c4aec768df6
Teaksters <arne.meijs@student.uva.nl>
2021-10-18 15:33:07 +0200

    final now

--- test.py
+++ test.py
@@ -4,16 +4,20 @@ import numpy as np
 import matplotlib.pyplot as plt
 import pickle
 
-all_r = []
-for i in range(10):
-    _, (len, res) = main.main(env=0, num_episodes=500000, epsilon=0.05,
+pName = "save.p"
+N = 50
+episodes = 300000
+all_r = np.zeros([episodes])
+for i in range(N):
+    _, (len, res) = main.main(env=0, num_episodes=episodes, epsilon=0.05,
                        q=True, dq=False)
-    all_r.append(res)
+    all_r += res
 
-mean_res = np.mean(all_r, axis=0)
-pickle.dump(mean_res, open( "save.p", "wb" ))
+mean_res = all_r / N
+print(mean_res)
+pickle.dump(mean_res, open(pName, "wb" ))
 
-mean_res = pickle.load( open( "save.p", "rb" ) )
+mean_res = pickle.load( open(pName, "rb" ) )
 
 x = np.arange(mean_res.shape[0])
 plt.plot(x, mean_res)
