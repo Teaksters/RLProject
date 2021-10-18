db19beb691d8629ee0c5131aaa393071dcd86e2b
Redouane Dahmani <redouane1997@hotmail.com>
2021-10-18 17:51:00 +0200

    env

---
+++ jobbert.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+#SBATCH --job-name=test
+#SBATCH --ntasks=1
+#SBATCH --cpus-per-task=4
+#SBATCH --time=24:00:00
+#SBATCH --mem=32000M
+#SBATCH --output=test.out
+
+module purge
+module load 2019
+module load Python/3.7.5-foss-2019b
+# module load CUDA/10.1.243
+# module load cuDNN/7.6.5.32-CUDA-10.1.243
+# module load NCCL/2.5.6-CUDA-10.1.243
+module load Anaconda3/2018.12
+
+# Your job starts in the directory where you call sbatch
+cd $HOME/
+# Activate your environment
+
+source activate rlcourse
+# Run your code
+srun python -u RLProject/test.py
--- test.py
+++ test.py
@@ -4,22 +4,27 @@ import numpy as np
 import matplotlib.pyplot as plt
 import pickle
 
+# pName = "Q_test_frozen_lake.p"
 pName = "Q_test_cliff_walking.p"
-N = 5
-episodes = 300000
+N = 100
+episodes = 500000
 all_r = np.zeros([episodes])
+all_r_dq = np.zeros([episodes])
 for i in range(N):
-    _, (len, res) = main.main(env=2, num_episodes=episodes, epsilon=0.05, q=True, dq=False)
+    # _, (len, res) = main.main(env=2, num_episodes=episodes, epsilon=0.05, q=True, dq=False)
     # _, _, (len, res) = main.main(env=0, num_episodes=episodes, epsilon=0.05, q=False, dq=True)
-    # _, (len, res), _, _, (len, res) = main.main(env=0, num_episodes=episodes, epsilon=0.05, q=True, dq=True)
+    _, (len, res), _, _, (len, res_dq) = main.main(env=0, num_episodes=episodes, epsilon=0.05, q=True, dq=True)
     all_r += res
+    all_r_dq += res_dq
 
 mean_res = all_r / N
-print(mean_res)
+mean_res_dq = all_r_dq / N
+
 pickle.dump(mean_res, open("pickle_files/" + pName, "wb" ))
+pickle.dump(mean_res_dq, open("pickle_files/D" + pName, "wb" ))
 
-mean_res = pickle.load( open("pickle_files/" + pName, "rb" ))
+# mean_res = pickle.load( open("pickle_files/" + pName, "rb" ))
 
-x = np.arange(mean_res.shape[0])
-plt.plot(x, mean_res)
-plt.show()
+# x = np.arange(mean_res.shape[0])
+# plt.plot(x, mean_res)
+# plt.show()
