dfe83905beedf3bbb4ab40c211edd0520bb440de
thomasmeeus2 <thomasmeeus@hotmail.com>
2021-10-18 16:48:20 +0200

    pickle print

---
+++ pickle_print.py
@@ -0,0 +1,17 @@
+import pickle
+import numpy as np
+import matplotlib.pyplot as plt
+
+# mean_DQ = pickle.load( open("pickle_files/DQ_test_frozen_lake.p", "rb" ))
+mean_Q = pickle.load( open("pickle_files/Q_test_cliff_walking.p", "rb" ))
+print(mean_Q)
+
+# x = np.arange(mean_DQ.shape[0])
+# plt.plot(x, mean_DQ)
+# plt.savefig("owyeah_DQ")
+# plt.show()
+
+x = np.arange(0, mean_Q.shape[0], 10000)
+plt.scatter(x, mean_Q[::10000])
+plt.savefig("owyeah_Q")
+plt.show()
--- test.py
+++ test.py
@@ -4,21 +4,21 @@ import numpy as np
 import matplotlib.pyplot as plt
 import pickle
 
-pName = "save.p"
+pName = "Q_test_cliff_walking.p"
 N = 5
 episodes = 300000
 all_r = np.zeros([episodes])
 for i in range(N):
-    _, (len, res) = main.main(env=0, num_episodes=episodes, epsilon=0.05, q=True, dq=False)
+    _, (len, res) = main.main(env=2, num_episodes=episodes, epsilon=0.05, q=True, dq=False)
     # _, _, (len, res) = main.main(env=0, num_episodes=episodes, epsilon=0.05, q=False, dq=True)
     # _, (len, res), _, _, (len, res) = main.main(env=0, num_episodes=episodes, epsilon=0.05, q=True, dq=True)
     all_r += res
 
 mean_res = all_r / N
 print(mean_res)
-pickle.dump(mean_res, open(pName, "wb" ))
+pickle.dump(mean_res, open("pickle_files/" + pName, "wb" ))
 
-mean_res = pickle.load( open(pName, "rb" ) )
+mean_res = pickle.load( open("pickle_files/" + pName, "rb" ))
 
 x = np.arange(mean_res.shape[0])
 plt.plot(x, mean_res)
