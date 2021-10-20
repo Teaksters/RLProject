eb9a87b5ff92deb2d62c071cd3548b3f9158670f
Redouane Dahmani <redouane1997@hotmail.com>
2021-10-20 13:42:17 +0200

    q learning init

--- main.py
+++ main.py
@@ -21,9 +21,10 @@ def main(env=0, num_episodes=50000, epsilon=0.05, q=True, dq=False, size=4, p=0.
     s_2_idx = None
     env_choice = env_opt[env] # Change this to change the env
     if env_choice == 'frozenLake':
-        env = FrozenLakeEnv(desc=None, map_name=None, is_slippery=False, size=size, p=p)
-        Q = np.zeros((env.nS, env.nA))
-        Q2 = np.zeros((env.nS, env.nA))
+        env = FrozenLakeEnv(desc=None, map_name=None, is_slippery=True, size=size, p=p)
+        Q = np.random.normal(5, 2, (env.nS, env.nA))
+        Q2 = np.random.normal(5, 2, (env.nS, env.nA))
+        # Q2 = np.zeros((env.nS, env.nA))
         policy = EpsilonGreedyPolicy(Q, epsilon, env.nA)
         policy2 = EpsilonGreedyPolicy(Q2, epsilon, env.nA)
         print('The generated map:')
