101be24b3995ab29b78d05820a4ed540446ae793
reneedecoolste <renee.oldenkamp@gmail.com>
2021-10-21 12:53:15 +0200

    ren

--- ren.py
+++ ren.py
@@ -14,19 +14,19 @@ mean_res, mean_res_dq = run_experiment_func(pName, N, episodes, size, number_hol
 pickle.dump(mean_res, open("pickle_files/" + pName, "wb" ))
 pickle.dump(mean_res_dq, open("pickle_files/D" + pName, "wb" ))
 
-print("***************")
-print("*CONFIG A DONE*")
-print("***************")
-
-# Config B
-pName       = ""
-N           = 100
-episodes    = 10000
-size        = 8
-p           = 5
-
-pName = "FrozenLake_N" + str(N) + "_epi" + str(episodes) + "_size" + str(size) + "_holes" + str(number_holes) + ".p"
-
-mean_res, mean_res_dq = run_experiment_func(pName, N, episodes, size, number_holes)
-pickle.dump(mean_res, open("pickle_files/" + pName, "wb" ))
-pickle.dump(mean_res_dq, open("pickle_files/D" + pName, "wb" ))
+# print("***************")
+# print("*CONFIG A DONE*")
+# print("***************")
+#
+# # Config B
+# pName       = ""
+# N           = 100
+# episodes    = 10000
+# size        = 8
+# p           = 5
+#
+# pName = "FrozenLake_N" + str(N) + "_epi" + str(episodes) + "_size" + str(size) + "_holes" + str(number_holes) + ".p"
+#
+# mean_res, mean_res_dq = run_experiment_func(pName, N, episodes, size, number_holes)
+# pickle.dump(mean_res, open("pickle_files/" + pName, "wb" ))
+# pickle.dump(mean_res_dq, open("pickle_files/D" + pName, "wb" ))
