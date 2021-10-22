5f778e1bf3d6d70357b374a5f0c765da7cb9b2eb
Redouane Dahmani <redouane1997@hotmail.com>
2021-10-22 19:22:43 +0200

    final

--- plot_results.py
+++ plot_results.py
@@ -2,15 +2,12 @@ import matplotlib.pyplot as plt
 import numpy as np
 import pickle
 
-# path = "pickle_files/Organized/"
-
 
 def extract_pickle(slippy, episodes, size, holes):
     mean_DQ = pickle.load(open("pickle_files/Organized/" + "DFrozenLake_" + slippy + "_N100_epi" + episodes + "_size" + size + "_holes" + holes + ".p", "rb"))
     mean_Q  = pickle.load(open("pickle_files/Organized/" + "FrozenLake_"  + slippy + "_N100_epi" + episodes + "_size" + size + "_holes" + holes + ".p", "rb"))
 
     return mean_DQ, mean_Q
-
 
 def loop_experiments():
     slippys = ["slippy", "noslippy"]
@@ -29,6 +26,8 @@ def loop_experiments():
                 else:
                     episodes = "50000"
 
+                if slippy == "noslippy" and size == "8" and hole > 6:
+                    continue
                 combinations.append([slippy, episodes, size, str(hole)])
 
     return combinations
@@ -39,55 +38,93 @@ def plot_all():
     slippy_4_DQ = []
     noslippy_4_Q = []
     noslippy_4_DQ = []
-    # slippy_8_Q = []
-    # slippy_8_DQ = []
-    # noslippy_8_Q = []
-    # noslippy_8_DQ = []
+    slippy_8_Q = []
+    slippy_8_DQ = []
+    noslippy_8_Q = []
+    noslippy_8_DQ = []
     for i in range(len(combinations)):
         slippy, episodes, size, hole = combinations[i]
-        # if slippy == "noslippy" and size == "8":
-        if size == "8":
-            continue
         mean_DQ, mean_Q = extract_pickle(slippy, episodes, size, hole)
         mean_DQ = np.mean(mean_DQ[-100:])
         mean_Q =  np.mean(mean_Q[-100:])
         if slippy == "slippy":
             if size == "4":
-                # print("ja1")
                 slippy_4_DQ.append(mean_DQ)
                 slippy_4_Q.append(mean_Q)
             else:
-                # print("ja2")
-                pass
-                # slippy_8_DQ.append(mean_DQ)
-                # slippy_8_Q.append(mean_Q)
+                slippy_8_DQ.append(mean_DQ)
+                slippy_8_Q.append(mean_Q)
         else:
             if size == "4":
-                # print("ja3")
                 noslippy_4_DQ.append(mean_DQ)
                 noslippy_4_Q.append(mean_Q)
             else:
-                # print("ja4")
-                pass
-                # noslippy_8_DQ.append(mean_DQ)
-                # noslippy_8_Q.append(mean_Q)
+                noslippy_8_DQ.append(mean_DQ)
+                noslippy_8_Q.append(mean_Q)
 
-    print(slippy_4_Q)
-    print(slippy_4_DQ)
-    print(noslippy_4_Q)
-    print(noslippy_4_DQ)
+    x = np.arange(1, 9)
     plt.figure(1)
-    plt.plot(slippy_4_Q, label="slippy_4_Q")
-    plt.plot(slippy_4_DQ, label="slippy_4_DQ")
+    plt.plot(x, slippy_4_Q, c="red", label="Q-learning")
+    plt.plot(x, slippy_4_DQ, c="blue", label="DQ-learning")
     plt.legend()
+    plt.savefig("slippy_4")
     plt.figure(2)
-    plt.plot(noslippy_4_Q, label="noslippy_4_Q")
-    plt.plot(noslippy_4_DQ, label="noslippy_4_DQ")
-    # plt.plot(slippy_8_Q)
-    # plt.plot(slippy_8_DQ)
-    # plt.plot(noslippy_8_Q)
-    # plt.plot(noslippy_8_DQ)
+    plt.plot(x, noslippy_4_Q, c="red", label="Q-learning")
+    plt.plot(x, noslippy_4_DQ, c="blue", label="DQ-learning")
     plt.legend()
+    plt.savefig("noslippy_4")
+    plt.figure(3)
+    plt.plot(x, slippy_8_Q, c="red", label="Q-learning")
+    plt.plot(x, slippy_8_DQ, c="blue", label="DQ-learning")
+    plt.legend()
+    plt.savefig("slippy_8")
+    plt.figure(4)
+    plt.plot(x[:-2], noslippy_8_Q, c="red", label="Q-learning")
+    plt.plot(x[:-2], noslippy_8_DQ, c="blue", label="DQ-learning")
+    plt.legend()
+    plt.savefig("noslippy_8")
     plt.show()
 
 plot_all()
+
+combinations = loop_experiments()
+
+# import os
+# print("TODO")
+# for combination in combinations:
+#     slippy, episodes, size, holes = combination
+#     dq = "pickle_files/Organized/" + "DFrozenLake_" + slippy + "_N100_epi" + episodes + "_size" + size + "_holes" + holes + ".p"
+#     q =  "pickle_files/Organized/" + "FrozenLake_" + slippy + "_N100_epi" + episodes + "_size" + size + "_holes" + holes + ".p"
+#     if not os.path.isfile(dq):
+#         print(combination)
+#
+#     else:
+#         plt.figure()
+#         mean_DQ, mean_Q = extract_pickle(slippy, episodes, size, holes)
+#
+#         STEP = 20
+#
+#         y_Q = []
+#         episode_mean_Q = []
+#         for i in range(len(mean_Q)):
+#             episode_mean_Q.append(mean_Q[i])
+#             if i % STEP == 0:
+#                 y_Q.append(sum(episode_mean_Q) / len(episode_mean_Q))
+#                 episode_mean_Q = []
+#
+#         y_DQ = []
+#         episode_mean_DQ = []
+#         for i in range(len(mean_DQ)):
+#             episode_mean_DQ.append(mean_DQ[i])
+#             if i % STEP == 0:
+#                 y_DQ.append(sum(episode_mean_DQ) / len(episode_mean_DQ))
+#                 episode_mean_DQ = []
+#
+#         # print(len(y_Q))
+#         x_Q = np.arange(1, len(y_Q) + 1) * STEP
+#         x_DQ = np.arange(1, len(y_DQ) + 1) * STEP
+#
+#         plt.scatter(x_Q, y_Q, color="r", alpha=0.5, label = "Q-learning")
+#         plt.scatter(x_DQ, y_DQ, color="b", alpha=0.5, label = "Double Q-learning")
+#         plt.legend()
+#         plt.savefig("preliminary_plots/" + size + "_" + holes + "_" + slippy)
--- red.py
+++ red.py
@@ -4,9 +4,9 @@ import pickle
 # Config A
 pName           = ""
 N               = 100
-episodes        = 50000
-size            = 4
-number_holes    = 5
+episodes        = 20000
+size            = 8
+number_holes    = 7
 
 pName = "FrozenLake_slippy_N" + str(N) + "_epi" + str(episodes) + "_size" + str(size) + "_holes" + str(number_holes) + ".p"
 
@@ -14,57 +14,57 @@ mean_res, mean_res_dq = run_experiment_func(pName, N, episodes, size, number_hol
 pickle.dump(mean_res, open("pickle_files/" + pName, "wb" ))
 pickle.dump(mean_res_dq, open("pickle_files/D" + pName, "wb" ))
 
-print("***************")
-print("*CONFIG A DONE*")
-print("***************")
-
-# Config B
-pName       = ""
-N           = 100
-episodes    = 50000
-size        = 4
-number_holes           = 6
-
-pName = "FrozenLake_slippy_N" + str(N) + "_epi" + str(episodes) + "_size" + str(size) + "_holes" + str(number_holes) + ".p"
-
-mean_res, mean_res_dq = run_experiment_func(pName, N, episodes, size, number_holes)
-pickle.dump(mean_res, open("pickle_files/" + pName, "wb" ))
-pickle.dump(mean_res_dq, open("pickle_files/D" + pName, "wb" ))
-
-print("***************")
-print("*CONFIG A DONE*")
-print("***************")
-
-# Config B
-pName       = ""
-N           = 100
-episodes    = 50000
-size        = 4
-number_holes           = 7
-
-pName = "FrozenLake_slippy_N" + str(N) + "_epi" + str(episodes) + "_size" + str(size) + "_holes" + str(number_holes) + ".p"
-
-mean_res, mean_res_dq = run_experiment_func(pName, N, episodes, size, number_holes)
-pickle.dump(mean_res, open("pickle_files/" + pName, "wb" ))
-pickle.dump(mean_res_dq, open("pickle_files/D" + pName, "wb" ))
-
-print("***************")
-print("*CONFIG A DONE*")
-print("***************")
-
-# Config B
-pName       = ""
-N           = 100
-episodes    = 50000
-size        = 4
-number_holes           = 8
-
-pName = "FrozenLake_slippy_N" + str(N) + "_epi" + str(episodes) + "_size" + str(size) + "_holes" + str(number_holes) + ".p"
-
-mean_res, mean_res_dq = run_experiment_func(pName, N, episodes, size, number_holes)
-pickle.dump(mean_res, open("pickle_files/" + pName, "wb" ))
-pickle.dump(mean_res_dq, open("pickle_files/D" + pName, "wb" ))
-
-print("***************")
-print("*CONFIG A DONE*")
-print("***************")
+# print("***************")
+# print("*CONFIG A DONE*")
+# print("***************")
+#
+# # Config B
+# pName       = ""
+# N           = 100
+# episodes    = 50000
+# size        = 4
+# number_holes           = 6
+#
+# pName = "FrozenLake_slippy_N" + str(N) + "_epi" + str(episodes) + "_size" + str(size) + "_holes" + str(number_holes) + ".p"
+#
+# mean_res, mean_res_dq = run_experiment_func(pName, N, episodes, size, number_holes)
+# pickle.dump(mean_res, open("pickle_files/" + pName, "wb" ))
+# pickle.dump(mean_res_dq, open("pickle_files/D" + pName, "wb" ))
+#
+# print("***************")
+# print("*CONFIG A DONE*")
+# print("***************")
+#
+# # Config B
+# pName       = ""
+# N           = 100
+# episodes    = 50000
+# size        = 4
+# number_holes           = 7
+#
+# pName = "FrozenLake_slippy_N" + str(N) + "_epi" + str(episodes) + "_size" + str(size) + "_holes" + str(number_holes) + ".p"
+#
+# mean_res, mean_res_dq = run_experiment_func(pName, N, episodes, size, number_holes)
+# pickle.dump(mean_res, open("pickle_files/" + pName, "wb" ))
+# pickle.dump(mean_res_dq, open("pickle_files/D" + pName, "wb" ))
+#
+# print("***************")
+# print("*CONFIG A DONE*")
+# print("***************")
+#
+# # Config B
+# pName       = ""
+# N           = 100
+# episodes    = 50000
+# size        = 4
+# number_holes           = 8
+#
+# pName = "FrozenLake_slippy_N" + str(N) + "_epi" + str(episodes) + "_size" + str(size) + "_holes" + str(number_holes) + ".p"
+#
+# mean_res, mean_res_dq = run_experiment_func(pName, N, episodes, size, number_holes)
+# pickle.dump(mean_res, open("pickle_files/" + pName, "wb" ))
+# pickle.dump(mean_res_dq, open("pickle_files/D" + pName, "wb" ))
+#
+# print("***************")
+# print("*CONFIG A DONE*")
+# print("***************")
