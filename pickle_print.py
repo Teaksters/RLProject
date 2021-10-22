7fabcf70b52ceeab807414d38dbb6291ca16bc7c
thomasmeeus2 <thomasmeeus@hotmail.com>
2021-10-22 09:56:22 +0200

    results

--- pickle_print.py
+++ pickle_print.py
@@ -2,8 +2,8 @@ import pickle
 import numpy as np
 import matplotlib.pyplot as plt
 
-mean_DQ = pickle.load( open("pickle_files/DFrozenLake_N100_epi20000_size8_holes1.p", "rb" ))
-mean_Q = pickle.load( open("pickle_files/FrozenLake_N100_epi20000_size8_holes1.p", "rb" ))
+mean_DQ = pickle.load( open("pickle_files/DFrozenLake_noslippy_N100_epi50000_size8_holes1.p", "rb" ))
+mean_Q = pickle.load( open("pickle_files/FrozenLake_noslippy_N100_epi50000_size8_holes1.p", "rb" ))
 print(mean_Q)
 
 # x = np.arange(mean_DQ.shape[0])
